f6df9028b48cc9d287e79f8c966f0567a39607ef
xramzesx <xramzesbusiness@gmail.com>
2022-12-26 02:34:10 +0100

    [map] add IWorldMap drafts with abstract class

--- src/main/java/oolab/darwin/maps/ToxicMap.java
+++ src/main/java/oolab/darwin/maps/ToxicMap.java
@@ -1,6 +1,8 @@
 package oolab.darwin.maps;
 
+import oolab.darwin.Config;
 import oolab.darwin.Vector2d;
+import oolab.darwin.interfaces.IMapBoundary;
 import oolab.darwin.interfaces.IMapElement;
 import oolab.darwin.interfaces.IPositionObservable;
 import oolab.darwin.interfaces.IWorldMap;
@@ -8,5 +10,10 @@ import oolab.darwin.interfaces.IWorldMap;
 import java.util.ArrayList;
 
 public class ToxicMap extends AbstractWorldMap {
-
+    public ToxicMap(
+        Config config,
+        IMapBoundary mapBoundary
+    ) {
+        super(config, mapBoundary);
+    }
 }
