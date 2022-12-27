50638a2a88684cd1884fe5a348252a69f9fe8e22
xramzesx <xramzesbusiness@gmail.com>
2022-12-27 17:52:12 +0100

    [boundary] introduce MapBoundaries

--- src/main/java/oolab/darwin/boundaries/AbstractBoundary.java
+++ src/main/java/oolab/darwin/boundaries/AbstractBoundary.java
@@ -2,10 +2,14 @@ package oolab.darwin.boundaries;
 
 import oolab.darwin.Config;
 import oolab.darwin.Vector2d;
+import oolab.darwin.elements.Animal;
+import oolab.darwin.enums.Border;
 import oolab.darwin.interfaces.IMapBoundary;
 
+import java.util.ArrayList;
+
 public abstract class AbstractBoundary implements IMapBoundary {
-    private final Config config;
+    protected final Config config;
 
     private final Vector2d startBorder;
     private final Vector2d endBorder;
@@ -19,11 +23,36 @@ public abstract class AbstractBoundary implements IMapBoundary {
         );
     }
 
+    @Override
     public Vector2d lowerLeft() {
         return this.startBorder;
     };
 
+    @Override
     public Vector2d upperRight() {
         return this.endBorder;
     }
+
+    @Override
+    public void checkPosition(Animal animal) {
+        ArrayList<Border> borders = new ArrayList<>();
+
+        if (animal.position.y < this.lowerLeft().y )
+            borders.add(Border.BOTTOM);
+        if ( animal.position.y > this.upperRight().y )
+            borders.add(Border.TOP);
+        if ( animal.position.x < this.lowerLeft().x )
+            borders.add(Border.LEFT);
+        if ( animal.position.x > this.upperRight().x )
+            borders.add(Border.RIGHT);
+
+        onBorders(animal, borders);
+    }
+
+    @Override
+    public void onBorders( Animal animal, ArrayList<Border> borders ) {
+        for ( Border border: borders )
+            onBorder(animal, border);
+    }
+
 }
