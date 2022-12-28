27c4c550e91d9d13e467dfa8c36cf11728435c02
xramzesx <xramzesbusiness@gmail.com>
2022-12-28 14:08:03 +0100

    [element] improve Plant

--- src/main/java/oolab/darwin/elements/Plant.java
+++ src/main/java/oolab/darwin/elements/Plant.java
@@ -1,13 +1,13 @@
 package oolab.darwin.elements;
 
+import oolab.darwin.Config;
 import oolab.darwin.Vector2d;
 
 public class Plant extends AbstractMapElement {
     public int energy;
-    public Vector2d position;
 
-
-    public Plant( int energy ){
-        this.energy = energy;
+    public Plant(Config config, Vector2d position){
+        this.energy = config.plantEnergy;
+        this.position = position;
     }
 }
