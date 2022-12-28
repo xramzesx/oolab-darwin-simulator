eb8ee10acc32a29b858956761a26a10b008affa0
xramzesx <xramzesbusiness@gmail.com>
2022-12-28 14:06:52 +0100

    [utils] add getRandomElement

--- src/main/java/oolab/darwin/Utils.java
+++ src/main/java/oolab/darwin/Utils.java
@@ -1,6 +1,7 @@
 package oolab.darwin;
 
 import java.util.Random;
+import java.util.Set;
 
 public class Utils {
     public static int getRandomInt( int start, int end ) {
@@ -29,6 +30,26 @@ public class Utils {
         return index >= current
                 ? (index + 1) % length
                 : index;
+    }
+
+    public static <T> T getRandomElement(Set<? extends T> set ) {
+        Random random = new Random();
+
+        int chosenIndex = getRandomInt(0, set.size());
+        int currentIndex = 0;
+
+        T element = null;
+
+        for (T e : set) {
+            element = e;
+
+            if (currentIndex == chosenIndex)
+                return element;
+
+            currentIndex++;
+        }
+
+        return element;
     }
 
 }
